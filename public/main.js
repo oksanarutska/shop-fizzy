df8e9d7bcc0bcb2101281ab4b905e79587ea13e6
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2020-01-18 19:12:31 +0200

    fixed block items product

--- public/main.js
+++ public/main.js
@@ -3,12 +3,15 @@ let landing = {
         this.itemsBlock = document.querySelector('.items-wrapper')
         this.itemActive = document.querySelector('.active')
         console.log(this.itemsList)
-        for (let i = 1; i < 50; i++) {
-            var div = document.createElement("div")
-            div.innerText = i
+        for (let i = 0; i < 51; i++) {
+            let div = document.createElement("div")
             div.setAttribute('data-price', i)
             div.classList.add('item')
-            this.itemsBlock.appendChild(div)
+            let newBlock = this.itemsBlock.appendChild(div)
+            let p = document.createElement("p")
+            p.innerText = i
+            p.classList.add('item_view')
+            newBlock.appendChild(p)
         }
         this.itemsList = document.querySelectorAll('.items-wrapper div')
 
@@ -22,14 +25,27 @@ let landing = {
 
         }.bind(this))
 
+        document.addEventListener('click', function(e) {
+            // console.log(e.target)
+            if (e.target.classList.contains('active') || e.target.classList.contains('item') || e.target.classList.contains('item_view')) {
+
+            } else {
+                this.itemsBlock.classList.remove('showDrop')
+            }
+        }.bind(this))
+
+
     },
 
     selectItem: function() {
         this.itemsBlock.addEventListener('click', function(e) {
             console.log(e.target)
-            let item = e.target
-            this.itemActive.innerHTML = ''
-            this.itemActive.appendChild(item)
+            if (e.target.classList.contains('item_view')) {
+                let item = e.target.textContent
+                this.itemActive.innerText = item
+            }
+
+
 
         }.bind(this))
 
@@ -38,4 +54,4 @@ let landing = {
 
 
 }
-landing.init()
+landing.init();
